21e069450fb13c648955542f4a0ffaa7467c0d46
fedelago31 <fede.lagomarsino@gmail.com>
2020-09-25 13:45:28 +0200

    Bug fix
    Fixed a bug which prevent loading

--- application.py
+++ application.py
@@ -35,10 +35,6 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
-
 
 @app.route("/")
 @login_required
